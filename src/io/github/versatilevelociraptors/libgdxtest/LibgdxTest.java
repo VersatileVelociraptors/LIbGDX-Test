b5793e0bb6ed973a529bd37f7c3237ad2854961b
GurgleFurg <advaniandrew@gmail.com>
2015-04-12 00:22:38 -0500

    added some text

--- src/io/github/versatilevelociraptors/libgdxtest/LibgdxTest.java
+++ src/io/github/versatilevelociraptors/libgdxtest/LibgdxTest.java
@@ -5,9 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.audio.Mp3.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LibgdxTest implements ApplicationListener{
@@ -19,10 +21,12 @@ public class LibgdxTest implements ApplicationListener{
 	public static final boolean COPS = true;
 	
 	private SpriteBatch batch;
+	private BitmapFont font;
 	private Texture texture;
 	private FPSLogger fps;
 	
 	private float r,g,b;
+	private double text_x, text_y;
 	
 	private int timer;
 	
@@ -51,6 +55,7 @@ public class LibgdxTest implements ApplicationListener{
 		texture = new Texture("assets/images/VersatileVelociraptors.png");
 		fps = new FPSLogger();
 		timer = 30;
+		font = new BitmapFont();
 		coco = (Sound) Gdx.audio.newSound(Gdx.files.internal("assets/music/coco.mp3"));
 		coco.play();
 	}
@@ -61,12 +66,18 @@ public class LibgdxTest implements ApplicationListener{
 			r = (float)Math.random();
 			g = (float)Math.random();
 			b = (float)Math.random();
+			
+			text_x = Math.random() * HEIGHT;
+			text_y = Math.random() * WIDTH;
+			
 			timer = 30;
 		}
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		Gdx.gl20.glClearColor(r, g, b, 1);
 		batch.begin();
 		batch.draw(texture , Gdx.input.getX() - texture.getWidth()/2, getHeight() - Gdx.input.getY() - texture.getHeight()/2/*had to subtract from height to invert the y axis*/);
+		font.setColor(new Color(1.0f - r, 1.0f - g, 1.0f - b, 1.0f));
+		font.draw(batch, "SMOKE WEED EVERYDAY", (int)text_x, (int)text_y);
 		batch.end();
 		fps.log();
 		timer--;
