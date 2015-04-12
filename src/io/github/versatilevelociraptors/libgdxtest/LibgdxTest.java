70f2f2f317710347c2d676c3dd3cc11e7310532a
LoadingPleaseWait <michael@murphey.org>
2015-04-11 22:31:50 -0500

    removed Game class, moved .gitignore and source files

--- src/io/github/versatilevelociraptors/libgdxtest/LibgdxTest.java
+++ src/io/github/versatilevelociraptors/libgdxtest/LibgdxTest.java
@@ -1,13 +1,15 @@
-package io.github.versatilevelociraptors;
+package io.github.versatilevelociraptors.libgdxtest;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Game implements ApplicationListener {
+public class LibgdxTest implements ApplicationListener{
 	
 	public static final String TITLE = "LibGDX-Test";
 	public static final int WIDTH = 640;
@@ -17,8 +19,24 @@ public class Game implements ApplicationListener {
 	private SpriteBatch batch;
 	private Texture texture;
 	private FPSLogger fps;
-	
 
+	public static void main(String[] agrs){
+		
+		LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+		
+		//disable frame limiter
+		configuration.vSyncEnabled = false;
+		configuration.foregroundFPS = 0;
+		configuration.backgroundFPS = 0;
+		
+		//set window options
+		configuration.title = TITLE;
+		configuration.width = WIDTH * SCALE;
+		configuration.height = HEIGHT * SCALE;
+		
+		new LwjglApplication(new LibgdxTest() , configuration);
+	}
+	
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
